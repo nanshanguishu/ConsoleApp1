8d4f8fbdcfbbd961e28274e986856a41841205e9
nanshanguishu <47811513+nanshanguishu@users.noreply.github.com>
2020-03-30 15:45:54 +0800

    添加项目文件。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 7.666666;
+            string b = "7.665";
+            Console.WriteLine(Math.Round(a, 2));
+            Console.WriteLine(String.Format("{0:F}", a));
+            Console.WriteLine(a.ToString("#0.00 "));
+            Console.WriteLine(Math.Round(b, 2));
+            Console.WriteLine(String.Format("{0:F}",b));
+            Console.WriteLine(b.ToString("#0.00 "));
+            Console.Read();
+        }
+    }
+}
